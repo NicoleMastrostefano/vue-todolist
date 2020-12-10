bbc50b0329f7d147468f3604a14e5b13028378c7
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-12-10 16:25:57 +0100

    js/css

---
+++ js/script.js
@@ -0,0 +1,29 @@
+Vue.component('todo-item', {
+  template: '\
+    <li>\
+      {{ title }}\
+      <button v-on:click="$emit(\'remove\')">X</button>\
+    </li>\
+  ',
+  props: ['title']
+})
+
+var app = new Vue({
+  el: '#root',
+  data: {
+    newTodoItem: '',
+    items: [
+      {title: 'Andare a fare la spesa'},
+      {title: 'Yoga'},
+      {title: 'Esercitazione boolean'}
+    ],
+  },
+  methods: {
+    addNewTodo: function () {
+      this.items.push({
+        title: this.newTodoItem
+      })
+       this.newTodoItem = ''
+    }
+  }
+})
